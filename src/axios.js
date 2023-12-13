c41c1ba9ccd0d4fdae674a81bce587fcd260e662
Abbas Guliyev <abbasquliyev111@gmail.com>
2023-12-13 11:20:46 +0400

    baseURL

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const access = localStorage.getItem('access')
 
 export default axios.create({
-    // baseURL: "http://188.132.202.212:8000/api/v1/",
-    baseURL: "http://localhost:8000/api/v1/",
+    baseURL: "http://188.132.202.212:8000/api/v1/",
+    // baseURL: "http://localhost:8000/api/v1/",
     headers: {
         "Content-Type" : "application/json",
         "Authorization" : `Bearer ${access}`
