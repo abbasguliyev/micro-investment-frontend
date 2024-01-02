5aae8bb3a5eb3808f6688cf0c328fe0d575e0d5f
abbasguliyev <abbasquliyev111@gmail.com>
2024-01-02 19:01:48 +0400

    new features

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className={`w-full flex justify-center items-center footer p-5 text-xs md:text-base`}>© 2023 <a href="https://www.linkedin.com/in/abbas-guliyev/" className='ml-2' target='_blank'>Abbas Guliyev</a>, Bütün hüquqlar qorunur.</footer>
+    <footer className={`w-full flex justify-center items-center footer p-5 text-xs md:text-base`}>© 2023 <a href="https://t.me/halalekosistem1" className='ml-2' target='_blank'>Halal Ekosistem</a>, Bütün hüquqlar qorunur.</footer>
   )
 }
 
